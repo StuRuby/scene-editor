527a668c2a029de04db6cba60023688f28ce422b
StuRuby <937931249@qq.com>
2020-06-30 13:01:11 +0800

    feature:添加sidebar 场景功能

---
+++ src/components/sidebar/scene/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Outliner } from './outliner';
+
+export function SidebarScene() {
+	return (
+		<React.Fragment>
+			<Outliner />
+		</React.Fragment>
+	);
+}
---
+++ src/components/sidebar/scene/outliner.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Card, Tree } from 'antd';
+import styled from 'styled-components';
+import { DownOutlined } from '@ant-design/icons';
+
+const { TreeNode } = Tree;
+const CardContainer = styled(Card)`
+	height: 300px;
+	overflow: scroll;
+`;
+
+export function Outliner() {
+	return (
+		<CardContainer>
+			<Tree defaultExpandAll={true} showLine>
+				<TreeNode title="parent 1-0" key="0-0-0">
+					<TreeNode title="leaf" key="0-0-0-0" />
+					<TreeNode title="leaf" key="0-0-0-1" />
+					<TreeNode title="leaf" key="0-0-0-2" />
+				</TreeNode>
+				<TreeNode title="parent 1-1" key="0-0-1"></TreeNode>
+				<TreeNode title="parent 1-2" key="0-0-2">
+					<TreeNode title="leaf" key="0-0-2-0" />
+					<TreeNode title="leaf" key="0-0-2-1" />
+				</TreeNode>
+				<TreeNode title="parent 1-0" key="0-0-0">
+					<TreeNode title="leaf" key="0-0-0-0" />
+					<TreeNode title="leaf" key="0-0-0-1" />
+					<TreeNode title="leaf" key="0-0-0-2" />
+				</TreeNode>
+				<TreeNode title="parent 1-1" key="0-0-1"></TreeNode>
+				<TreeNode title="parent 1-2" key="0-0-2">
+					<TreeNode title="leaf" key="0-0-2-0" />
+					<TreeNode title="leaf" key="0-0-2-1" />
+				</TreeNode>
+				<TreeNode title="parent 1-0" key="0-0-0">
+					<TreeNode title="leaf" key="0-0-0-0" />
+					<TreeNode title="leaf" key="0-0-0-1" />
+					<TreeNode title="leaf" key="0-0-0-2" />
+				</TreeNode>
+				<TreeNode title="parent 1-1" key="0-0-1"></TreeNode>
+				<TreeNode title="parent 1-2" key="0-0-2">
+					<TreeNode title="leaf" key="0-0-2-0" />
+					<TreeNode title="leaf" key="0-0-2-1" />
+				</TreeNode>
+			</Tree>
+		</CardContainer>
+	);
+}
